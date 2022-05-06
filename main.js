efe2eb6b57edbd8f0e1b2e18476bdbfa05ef0ee5
bunny <67633271+bunnykek@users.noreply.github.com>
2022-05-07 01:29:36 +0530

    Gets notification when the chatbox is being spammed.
    Removed the /chatbox command

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const TelegramBot = require('node-telegram-bot-api');
 var fs = require('fs');
+const { type } = require('os');
 require('dotenv').config()
 
 const logo =
@@ -21,13 +22,15 @@ const token = process.env.bot_token;
 const Cookie = JSON.parse(process.env.cookie);
 const user_id = process.env.user_id;
 const threshold = parseInt(process.env.threshold);
+var InterVar = null;
+var lastIndex = 0;
 
 async function main() {
 
   const browser = await puppeteer.launch({
     headless: true,
     ignoreDefaultArgs: ['--enable-automation'],
-    args: ['--start-maximized','--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--start-maximized', '--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const context = browser.defaultBrowserContext();
@@ -36,21 +39,39 @@ async function main() {
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
   await page.setCookie(...Cookie);
-  await page.setViewport({ width: 1366, height: 768});
+  await page.setViewport({ width: 1366, height: 768 });
   let restrict = 0;
 
-  setInterval(async () => {
+  // Create a bot that uses 'polling' to fetch new updates
+  const bot = new TelegramBot(token, { polling: true });
+
+  async function checking() {
+    //console.log("cycled");
     try {
+      //Checks if the chat is being spammed
+      console.log("checking the chat");
+      let count = 0;
+      const arr = await page.evaluate(() => [...document.querySelectorAll("[data-formatted-timestamp]")].map(elem => elem.attributes[9].nodeValue))
+      if (arr.length - lastIndex >= 15) {
+        console.log("The class chatbox is being spammed!");
+        bot.sendMessage(user_id, 'The class chatbox is being spammed!')
+        bot.sendMessage(user_id, 'The class chatbox is being spammed!!')
+      }
+      lastIndex = arr.length - 1;
+      //console.log("checked the chat");
+
+      //Checks the class strength 
+      //console.log("checking the class strength");
       await page.waitForSelector('.uGOf1d')
       let element = await page.$('.uGOf1d')
       let value = await page.evaluate(el => el.textContent, element)
       if (parseInt(value) < threshold) {
         await bot.sendMessage(user_id, 'Meet strength has become less than the threshold strength.');
-
+        clearInterval(InterVar)
         await page.screenshot({ path: 'example.png' });
         let stream = await fs.createReadStream('./example.png');
         bot.sendPhoto(user_id, stream);
-        
+
         await bot.sendMessage(user_id, 'Leaving the meet...');
         let xpath = '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[10]/div[2]/div/div[7]/span/button'
         await page.waitForTimeout(1000)
@@ -64,41 +85,42 @@ async function main() {
           bot.sendPhoto(user_id, stream);
         }
       }
+      //console.log("checked the class strength");
     } catch {
       return null;
     }
-  },60000)
+  }
 
-  // Create a bot that uses 'polling' to fetch new updates
-  const bot = new TelegramBot(token, { polling: true });
-
-  //bot.on("polling_error", console.log);
-
-  // Matches "/echo [whatever]"
   bot.onText(/\/join (.+)/, async (msg, match) => {
-    if(msg.from.id != user_id) {
+    if (msg.from.id != user_id) {
       return null;
     }
 
-    if(restrict == 1) {
+    if (restrict == 1) {
       return null;
     }
     else {
       restrict = 1;
     }
+    lastIndex = 0;
     const chatId = msg.chat.id;
     const resp = match[1]; // the captured "whatever"
+
+    if (InterVar != null) {
+      clearInterval(InterVar)
+    }
+
     let classCode = ''
-    try{
+    try {
       classCode = resp.match(/\w+-\w+-\w+/)[0];
-    } catch{
+    } catch {
       await bot.sendMessage(chatId, 'Check your meeting code.\nMake sure that you have used the correct meet url.', { reply_to_message_id: msg.message_id })
       return null;
     }
-      console.log('Meet link : ', `https://meet.google.com/${classCode}`);
-      await page.goto(`https://meet.google.com/${classCode}`);
-      await bot.sendMessage(chatId, 'Please wait...', { reply_to_message_id: msg.message_id })
-    
+    console.log('Meet link : ', `https://meet.google.com/${classCode}`);
+    await page.goto(`https://meet.google.com/${classCode}`);
+    await bot.sendMessage(chatId, 'Please wait...', { reply_to_message_id: msg.message_id })
+
 
     let [button] = await page.$x("//button[contains(., 'Return to home screen')]");
     if (button) {
@@ -106,23 +128,23 @@ async function main() {
     }
 
     else {
-      
+
       //dismiss button 1
-      try{
+      try {
         xpath = '//*[@id="yDmH0d"]/div[3]/div[2]/div/div[2]/button'
         await page.waitForTimeout(3000)
         await page.keyboard.press('Enter')
-      } catch {}
+      } catch { }
 
       await page.waitForTimeout(3000)
 
       //dismiss button 2
-      try{
+      try {
         xpath = '//*[@id="yDmH0d"]/div[3]/div[2]/div/div[2]/button'
         await page.waitForTimeout(2000)
         await page.keyboard.press('Enter')
-      } catch {}
-      
+      } catch { }
+
       console.log("2nd dismiss button clicked")
 
       //Join button
@@ -135,7 +157,7 @@ async function main() {
       console.log("Join button clicked")
 
 
-      await page.keyboard.press('Tab', {delay: 5000})
+      await page.keyboard.press('Tab', { delay: 5000 })
       await page.keyboard.press('Tab')
 
       await page.waitForTimeout(5000)
@@ -143,6 +165,17 @@ async function main() {
       let stream = await fs.createReadStream('./example.png');
       bot.sendMessage(chatId, 'Requested/joined the class!', { reply_to_message_id: msg.message_id })
       bot.sendPhoto(chatId, stream);
+
+      //toggle chatbox
+      xpath = '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[10]/div[3]/div[3]/div/div/div[3]/span/button'
+      await page.waitForTimeout(1000)
+      elements = await page.$x(xpath)
+      if (elements.length != 0) {
+        await elements[0].click()
+        await page.waitForTimeout(1000)
+      }
+
+      InterVar = setInterval(checking, 60000);
     }
 
     restrict = 0;
@@ -161,7 +194,7 @@ async function main() {
       await page.waitForTimeout(2000);
       await page.keyboard.press('Backspace');
       await page.type("#bfTqV", resp, { delay: 300 });
-     
+
       xpath = '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[4]/div[2]/div[2]/div/div[5]/span/button';
       await page.waitForXPath(xpath)
       elements = await page.$x(xpath)
@@ -174,12 +207,12 @@ async function main() {
       bot.sendPhoto(chatId, stream);
     }
     catch {
-        bot.sendMessage(chatId, 'Send /chatbox first.', { reply_to_message_id: msg.message_id });
-      }
+      bot.sendMessage(chatId, 'Send /chatbox first.', { reply_to_message_id: msg.message_id });
+    }
   })
 
   bot.on('message', async (msg) => {
-    if(msg.from.id != user_id) {
+    if (msg.from.id != user_id) {
       return null;
     }
     const chatId = msg.chat.id;
@@ -198,6 +231,7 @@ async function main() {
         await elements[0].click()
         console.log('meet left');
         await page.waitForTimeout(2000)
+        clearInterval(InterVar)
         bot.sendMessage(chatId, 'Left the meet!', { reply_to_message_id: msg.message_id })
         await page.screenshot({ path: 'example.png' });
         let stream = await fs.createReadStream('./example.png');
@@ -208,27 +242,9 @@ async function main() {
       }
     }
 
-    else if(msg.text == '\/chatbox') {
-      console.log(msg.text);
-      let xpath = '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[10]/div[3]/div[3]/div/div/div[3]/span/button'
-      await page.waitForTimeout(1000)
-      let elements = await page.$x(xpath)
-      if (elements.length != 0) {
-        await elements[0].click()
-        await page.waitForTimeout(1000)
-        await page.screenshot({ path: 'example.png' });
-        let stream = await fs.createReadStream('./example.png');
-        bot.sendPhoto(chatId, stream);
-      }
-      else{
-        bot.sendMessage(chatId,'/join a meet first.',{ reply_to_message_id: msg.message_id });
-      }
-    }
-
     else if (msg.text == '\/help') {
       let help = `List of available commands:
 /join {gmeet_link}
-/chatbox (toggle chatbox)
 /message {test message} (sends message to chatbox)
 /status (shows the ongoing meet current status)
 /leave (leaves the current gmeet session)
